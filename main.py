4003d3864bdf99e4366a4bb0d09efc26e2054d15
Aria <aria@shrt.wtf>
2024-06-06 11:42:22 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+type = input("Do you want to add, minus, multiply, or divide? ")
+
+if type == "add":
+  firstNumber = input("What is the first number? ")
+  secondNumber = input("What is the second number? ")
+  answer = int(firstNumber) + int(secondNumber)
+  print(answer)
+  
+if type == "minus":
+  firstNumber = input("What is the first number? ")
+  secondNumber = input("What is the second number? ")
+  answer = int(firstNumber) - int(secondNumber)
+  int(firstNumber) / int(secondNumber)
+  print(answer)
+  
+if type == "multiply":
+  firstNumber = input("What is the first number? ")
+  secondNumber = input("What is the second number? ")
+  answer = int(firstNumber) * int(secondNumber)
+  int(firstNumber) / int(secondNumber)
+  print(answer)
+  
+if type == "divide":
+  firstNumber = input("What is the first number? ")
+  secondNumber = input("What is the second number? ")
+  answer = int(firstNumber) / int(secondNumber)
+  print(answer)
