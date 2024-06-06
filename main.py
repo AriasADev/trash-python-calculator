c3c412e4913c6dba29da79f9b66f3325202ba6d5
Aria <aria@shrt.wtf>
2024-06-06 15:09:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,12 @@ if type == "minus":
   firstNumber = input("What is the first number? ")
   secondNumber = input("What is the second number? ")
   answer = int(firstNumber) - int(secondNumber)
-  int(firstNumber) / int(secondNumber)
   print(answer)
   
 if type == "multiply":
   firstNumber = input("What is the first number? ")
   secondNumber = input("What is the second number? ")
   answer = int(firstNumber) * int(secondNumber)
-  int(firstNumber) / int(secondNumber)
   print(answer)
   
 if type == "divide":
